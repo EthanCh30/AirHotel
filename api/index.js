93962f374ea4fccd4105b84862137e3c79ad615b
Ethan Chen <94731614+EthanCh30@users.noreply.github.com>
2024-03-22 02:46:03 +1030

    Update index.js

--- api/index.js
+++ api/index.js
@@ -17,7 +17,8 @@ const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 //app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(cors (
 {
-origin: ["https://blog-mern-api3.vercel.app"],
+//origin: ["https://blog-mern-api3.vercel.app"],
+origin: ["https://blog-mern-client.vercel.app/"],
 methods: ["POST", "GET"],
 credentials: true
 }
