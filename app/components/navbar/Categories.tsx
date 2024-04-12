81be02bb29d644b8eabb51e69265549fdc8fe5f5
chen <565306120@qq.com>
2024-04-12 20:03:18 +0930

    update Categories.tsx and logo.png

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -33,11 +33,6 @@ export const categories = [
     description: 'This property is has windmills!',
   },
   {
-    label: 'Modern',
-    icon: MdOutlineVilla,
-    description: 'This property is modern!'
-  },
-  {
     label: 'Countryside',
     icon: TbMountain,
     description: 'This property is in the countryside!'
@@ -53,16 +48,6 @@ export const categories = [
     description: 'This property is on an island!'
   },
   {
-    label: 'Lake',
-    icon: GiBoatFishing,
-    description: 'This property is near a lake!'
-  },
-  {
-    label: 'Skiing',
-    icon: FaSkiing,
-    description: 'This property has skiing activies!'
-  },
-  {
     label: 'Castles',
     icon: GiCastle,
     description: 'This property is an ancient castle!'
@@ -76,26 +61,6 @@ export const categories = [
     label: 'Camping',
     icon: GiForestCamp,
     description: 'This property offers camping activities!'
-  },
-  {
-    label: 'Arctic',
-    icon: BsSnow,
-    description: 'This property is in arctic environment!'
-  },
-  {
-    label: 'Desert',
-    icon: GiCactus,
-    description: 'This property is in the desert!'
-  },
-  {
-    label: 'Barns',
-    icon: GiBarn,
-    description: 'This property is in a barn!'
-  },
-  {
-    label: 'Lux',
-    icon: IoDiamond,
-    description: 'This property is brand new and luxurious!'
   }
 ]
 
